dc07d34939638f90ea0e8c7d25fcf18232e581dd
[luan.alves] <[luan-ad-daniel@hotmail.com]>
2019-10-22 18:42:01 -0300

    referenciando os elementos da DOM no javascript inclusive a lista de todos

--- todo_list/todos.js
+++ todo_list/todos.js
@@ -1,1 +1,13 @@
-alert('Funcionou!');
+// Referenciando os elemendo da DOM
+var listElement = document.querySelector('#app ul'); //Lista de todos
+var inputElement = document.querySelector('#app input'); //Campo input
+var buttonElement = document.querySelector('#app button'); //Botão para inserir o todo
+
+ var todos = [
+	'Estudar Javascript',
+	'Estudar React',
+	'Fazer café',
+	'Estudar React Native'
+ ];
+
+ 
