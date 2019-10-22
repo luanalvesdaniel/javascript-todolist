d0f25f554c1952921ce6d2d2525e87df3ef1dda4
[luan.alves] <[luan-ad-daniel@hotmail.com]>
2019-10-22 18:50:57 -0300

    renderizar os todos em tela atribuindo os mesmos em um elemento de todo

--- todo_list/todos.js
+++ todo_list/todos.js
@@ -1,13 +1,28 @@
 // Referenciando os elemendo da DOM
-var listElement = document.querySelector('#app ul'); //Lista de todos
-var inputElement = document.querySelector('#app input'); //Campo input
-var buttonElement = document.querySelector('#app button'); //Botão para inserir o todo
+var listElement = document.querySelector('#app ul'); // Lista de todos
+var inputElement = document.querySelector('#app input'); // Campo input
+var buttonElement = document.querySelector('#app button'); // Botão para inserir o todo
 
+// Array de string contendo a lista de todos
  var todos = [
-	'Estudar Javascript',
-	'Estudar React',
 	'Fazer café',
+	'Estudar Javascript',
+	'Tomar café',
+	'Estudar React',
+	'Tomar café',
 	'Estudar React Native'
  ];
 
- 
+// Renderizar os todos em tela
+function renderTodos(){
+	// For para array de strings, onde percorrerá cada linha do array
+	for (todo of todos){
+		var todoElement = document.createElement('li');
+		var todoText = document.createTextNode(todo);
+
+		todoElement.appendChild(todoText);
+		listElement.appendChild(todoElement);
+	}
+}
+
+renderTodos();
