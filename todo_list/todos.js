d44d954611d63de33e776f101939ceada3b5bdab
[luan.alves] <[luan-ad-daniel@hotmail.com]>
2019-11-05 18:55:35 -0300

    ajuste para criar os elementos no javascript

--- todo_list/todos.js
+++ todo_list/todos.js
@@ -1,7 +1,48 @@
-// Referenciando os elemendo da DOM
-var listElement = document.querySelector('#app ul'); // Lista de todos
-var inputElement = document.querySelector('#app input'); // Campo input
-var buttonElement = document.querySelector('#app button'); // Botão para inserir o todo
+// Buscar elementos pai
+var head = document.head;
+var body = document.body;
+
+// Título
+var titleElement = document.createElement('title');
+var texto = document.createTextNode('Todo List');
+titleElement.appendChild(texto);
+head.appendChild(titleElement);
+
+// Div
+var divElement = document.createElement('div');
+divElement.setAttribute('class','container');
+divElement.setAttribute('id','app');
+body.appendChild(divElement);
+
+// Input
+var inputE = document.createElement('input');
+var inputEText = document.createTextNode('');
+inputE.setAttribute('type','text');
+inputE.setAttribute('placeholder','Digite um todo');
+inputE.appendChild(inputEText);
+divElement.appendChild(inputE);
+
+// Button
+var buttonE = document.createElement('button');
+var buttonEText = document.createTextNode('Adicionar');
+buttonE.setAttribute('class','button');
+buttonE.appendChild(buttonEText);
+divElement.appendChild(buttonE);
+
+// h3
+var h3ElementTodo = document.createElement('h3');
+var h3TextTodo = document.createTextNode('Todo');
+h3ElementTodo.appendChild(h3TextTodo);
+divElement.appendChild(h3ElementTodo);
+
+var h3ElementCompleted = document.createElement('h3');
+var h3TextCompleted = document.createTextNode('Completed');
+h3ElementCompleted.appendChild(h3TextCompleted);
+divElement.appendChild(h3ElementCompleted);
+
+// ul
+var listElement = document.createElement('ul'); // Lista de todos
+h3ElementTodo.appendChild(listElement);
 
 // Array de string contendo a lista de todos
 // Iniciando com os dados o localstorage
@@ -11,9 +52,9 @@ var buttonElement = document.querySelector('#app button'); // Botão para inserir
 function renderTodos(){
 	// Colocar tudo dentro do ul como vazio
 	listElement.innerHTML = '';
-
 	// For para array de strings, onde percorrerá cada linha do array
 	for (todo of todos){
+		
 		var todoElement = document.createElement('li');
 		var todoText = document.createTextNode(todo);
 
@@ -40,18 +81,18 @@ renderTodos();
 
 // Adicionando novos todos
 function addTodo(){
-	var todoText = inputElement.value;
+	var todoText = inputE.value;
 	if (todoText === ''){
 		alert('Digite um todo')		
 	} else {
 		todos.push(todoText);
-		inputElement.value = '';
+		inputE.value = '';
 		renderTodos();
 		saveToStorage();
 	}	
 }
 
-buttonElement.onclick = addTodo;
+buttonE.onclick = addTodo;
 
 // Remoção de todos ao clicar em Excluir
 function deleteTodo(pos){
